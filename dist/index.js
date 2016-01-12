a4dd3cc4708ffa3321481fabeb2cd46a2bfd0736
Pablo Sichert <mail@pablosichert.de>
2016-01-12 15:46:26 +0100

    Simplify API

--- dist/index.js
+++ dist/index.js
@@ -4,49 +4,62 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
 var _crypto = require('crypto');
 
-var RandomHash = (function () {
-  function RandomHash() {
-    var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+function RandomHashFactory() {
+  var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
 
-    var _ref$hashLength = _ref.hashLength;
-    var hashLength = _ref$hashLength === undefined ? 6 : _ref$hashLength;
-    var _ref$charset = _ref.charset;
-    var charset = _ref$charset === undefined ? 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_' : _ref$charset;
+  var _ref$hashLength = _ref.hashLength;
+  var hashLength = _ref$hashLength === undefined ? 6 : _ref$hashLength;
+  var _ref$charset = _ref.charset;
+  var charset = _ref$charset === undefined ? 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_' : _ref$charset;
 
-    _classCallCheck(this, RandomHash);
+  var RandomHash = function RandomHash() {
+    var opts = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
 
-    this.hashLength = hashLength;
-    this.charset = charset;
-  }
-
-  // Contribution to Moritz Sichert https://gist.github.com/MoritzS/16c820f5e2d6132d7040
-
-  _createClass(RandomHash, [{
-    key: 'generate',
-    value: function generate() {
-      var byteLength;
-      var hash;
-      var bytes;
-
-      byteLength = Math.ceil(this.hashLength / 4) * 3;
-      bytes = (0, _crypto.randomBytes)(byteLength);
-      hash = bytesToChars(bytes, this.charset);
-      hash = hash.slice(0, this.hashLength);
-
-      return hash;
+    if (opts.hashLength) {
+      hashLength = opts.hashLength;
     }
-  }]);
+    if (opts.charset) {
+      charset = opts.charset;
+    }
+
+    var byteLength;
+    var hash;
+    var bytes;
+
+    byteLength = Math.ceil(hashLength / 4) * 3;
+    bytes = (0, _crypto.randomBytes)(byteLength);
+    hash = bytesToChars(bytes, charset);
+    hash = hash.slice(0, hashLength);
+
+    return hash;
+  };
+
+  Object.defineProperty(RandomHash, 'hashLength', {
+    get: function get() {
+      return hashLength;
+    },
+    set: function set(newHashLength) {
+      hashLength = newHashLength;
+    }
+  });
+
+  Object.defineProperty(RandomHash, 'charset', {
+    get: function get() {
+      return charset;
+    },
+    set: function set(newCharset) {
+      charset = newCharset;
+    }
+  });
 
   return RandomHash;
-})();
+}
 
-exports['default'] = RandomHash;
+exports['default'] = RandomHashFactory;
+
+// Credit to Moritz Sichert https://gist.github.com/MoritzS/16c820f5e2d6132d7040
 function bytesToChars(randomBytes, charset) {
   var bufferLength;
   var output;
