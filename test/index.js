38bb5b52cb658fc55a2867905ced9e71d6a53301
Pablo Sichert <mail@pablosichert.de>
2016-09-25 16:04:02 +0200

    Test bytesToChars

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
+import { randomBytes } from 'crypto';
 import expect from 'unexpected';
 
 import {
     generateHash,
+    bytesToChars,
     CharsetLengthError
 } from '../lib';
 
@@ -70,5 +72,69 @@ describe('generateHash', () => {
                 }
             });
         });
+    });
+});
+
+describe('bytesToChars', () => {
+    const arrayToBytes = array => {
+        const bits = Array.from(array.reduce((prev, curr) => {
+            return prev + curr;
+        }, ''));
+
+        const packs = [];
+
+        while (bits.length) {
+            let byte = bits.splice(0, 8);
+            byte = Number('0b' + byte.join(''));
+
+            packs.push(byte);
+        }
+
+        return packs;
+    };
+
+    it('should convert an iterable containing bytes to chars', () => {
+        const selectFromChars = ['000001', '000010', '000011', '000100'];
+
+        const randomIterable = arrayToBytes(selectFromChars);
+        // ['00000100', '00100000', '11000100']
+
+        const charset = {
+            length: 64,
+            0b000001/* 1 */: 'A',
+            0b000010/* 2 */: 'B',
+            0b000011/* 3 */: 'C',
+            0b000100/* 4 */: 'D'
+        };
+
+        const chars = bytesToChars(randomIterable, charset);
+
+        expect(chars, 'to satisfy', ['A', 'B', 'C', 'D']);
+    });
+
+    it('should pick from any charset that has a getter and length 64', () => {
+        const randomIterable = Buffer.from([1, 2, 3]);
+
+        const charset = new Proxy({
+            length: 64
+        }, {
+            get: (target, name) => {
+                return target[name] || 'foo';
+            }
+        });
+
+        const chars = bytesToChars(randomIterable, charset);
+
+        expect(chars, 'to satisfy', ['foo', 'foo', 'foo', 'foo']);
+    });
+
+    it('should throw if charset does not contain exactly 64 characters', () => {
+        const charset = ['a, b, c'];
+
+        try {
+            bytesToChars(randomBytes(1), charset);
+        } catch (error) {
+            expect(error, 'to be a', CharsetLengthError);
+        }
     });
 });
